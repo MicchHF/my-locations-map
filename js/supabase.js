dd66c90dec0b887192008f4c567c7566ca07b748
MicchHF <103744552+MicchHF@users.noreply.github.com>
2026-08-24 16:47:30 +0300

    Add files via upload

---
+++ js/supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://cwkgylbtcjfmweoldhyb.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_KnWO20wlfuWd0NYKMNKRvw_l1rttjaT';
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
